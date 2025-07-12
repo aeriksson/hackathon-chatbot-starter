b364cd2a2f8e80a671761787d84b8dbd7c52aa54
André Eriksson <aeriksson@users.noreply.github.com>
2025-07-12 21:58:48 +0200

    update frontend dependencies

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './app.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,5 +12,3 @@ root.render(
         <App />
     </>
 );
-
-reportWebVitals();
